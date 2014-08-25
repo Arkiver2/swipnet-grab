6db152875b4ecec9f1e2a85e5d31d91d0d5f316a
Arkiver2 <Arkiver@hotmail.com>
2014-08-25 22:14:35 +0200

    swipnet.lua: exclude urls with irc.swipnet.se

--- swipnet.lua
+++ swipnet.lua
@@ -26,6 +26,8 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
     return false
   elseif string.match(url, "www%.swipnet%.se/") then
     return false
+  elseif string.match(url, "irc%.swipnet%.se/") then
+    return false
   elseif string.match(url, "//////////") then
     return false
   elseif string.match(url, "swipnet%.se/([^/]+)/") then
